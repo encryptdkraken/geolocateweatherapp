3a77670446cfa884d737d72411489b367c91c655
encryptdkraken <54143526+encryptdkraken@users.noreply.github.com>
2021-03-03 14:07:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const notifcationElement = document.querySelector(".notification");
-const myform = document.getElementById(".searchweath");
 
 
 // App data
